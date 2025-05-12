6c90d410f617afd2d890c899d084e2b9d623b3e3
Furry Yang <furryyang@gmail.com>
2025-05-12 08:47:30 +0800

    setup workspace

---
+++ setup.sh
@@ -0,0 +1,12 @@
+export RISCV_DV_ROOT=$(pwd)
+export RISCV_TOOLCHAIN=/opt/riscv
+export RISCV_GCC="$RISCV_TOOLCHAIN/bin/riscv32-unknown-elf-gcc"
+export RISCV_OBJCOPY="$RISCV_TOOLCHAIN/bin/riscv32-unknown-elf-objcopy"
+
+export SPIKE_PATH=$RISCV_TOOLCHAIN/bin
+
+# export SAIL_RISCV="xx/xxx/"
+# export OVPSIM_PATH=/xx/xxx/riscv-ovpsim/bin/Linux64
+# export WHISPER_ISS="xx/xxx/swerv-ISS/build-Linux/whisper"
+alias run="python run.py"
+alias cov="python cov.py"
